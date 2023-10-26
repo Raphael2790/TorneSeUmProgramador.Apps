895f1d9d98efd614f91dc0f35f0c4754c70d7930
Raphael Silvestre dos Santos <raphael.silvestre-ext@viavarejo.com.br>
2023-10-25 22:22:50 -0300

    Troca de mensagens entre telas do app com MVVM Toolkit, editando e pesquisando tarefas

---
+++ Todo.TorneSeUmProgramador.App/Messages/NovaTarefaMessage.cs
@@ -0,0 +1,11 @@
+﻿using CommunityToolkit.Mvvm.Messaging.Messages;
+using Todo.TorneSeUmProgramador.Core.Modelos;
+
+namespace Todo.TorneSeUmProgramador.App.Messages;
+
+public class NovaTarefaMessage : ValueChangedMessage<Tarefa>
+{
+    public NovaTarefaMessage(Tarefa value) : base(value)
+    {
+    }
+}
--- Todo.TorneSeUmProgramador.Core/Modelos/Tarefa.cs
+++ Todo.TorneSeUmProgramador.Core/Modelos/Tarefa.cs
@@ -2,6 +2,7 @@
 
 public class Tarefa
 {
+    public Guid Id { get; set; } = Guid.NewGuid();
     public string Nome { get; set; } = string.Empty;
     public string Descricao { get; set; } = string.Empty;
     public DateTime DataConclusao { get; set; }
